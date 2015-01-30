1c8af7432f5f04639eff2508b1818c38bad7f88e
Hugh J. Devore <hughdevore@yahoo.com>
2015-01-29 23:52:36 -0600

    Added changed to layout, message board and js display order

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 		$.get(
 			'https://group-message4792.herokuapp.com/users.json',
 			function(users) {
+				
+				// users = users.keys(obj).sort().reverse();
 				render(users);
 			},
 			'json'
@@ -11,6 +13,26 @@ $(document).ready(function() {
 	};
 
 	var render = function(user) {
+
+		// var user = function(user) {
+		// 	// for (var i = 0; i < user.length; i++) {
+		// 	// 	var stringify = require('json-stable-stringify');
+
+		// 	// 	var s = stringify(user, function(a. b) {
+		// 	// 		return a.key < b.key ? 1 : -1;
+		// 	// 	}
+
+
+		// 	// }
+
+		// 	user.reverse();
+		// 	$.each(user, function(i, item) {
+		// 	    //do something with the item
+		// 	    console.log(item); 
+		// 	});
+
+		// }();
+
 		var messageRow = _.template('<div class="instance"><div class="user">User:</div><div class="name"><%= username %></div><div class="message">Message:</div><div class="content"><%= message %></div><div class="stamp">Time:</div><div class="time"><%= created_at %></div></div>')
 
 		
